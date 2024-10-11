67650827748f40c19e2b7ab6a5820877ca541e9b
Merel Ohler <merel.ohler@gmail.com>
2024-10-11 14:23:41 -0600

    connect fe to be

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,6 +9,19 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'DatingApp';
+  http = inject(HttpClient);
+  users: any;
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: (res:any) => {
+        this.users = res;
+        console.log(res);
+      },
+      error: (error) => {console.log(error)},
+      complete: () => {console.log('done!')}
+    });
+  }
 }
